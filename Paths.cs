efc71c0f93a5c348e1e0f29bc46af573d8b33887
placeholder <julianabaroa38@gmail.com>
2025-08-25 16:24:36 -0400

    fix (paths): second try

--- Paths.cs
+++ Paths.cs
@@ -1,21 +1,35 @@
+using System.Runtime.InteropServices;
+
 namespace DBStats;
 
 public static class Paths
 {
-#if LINUX
-    const string BASE_PATH = "/media/pi/MiDiscoExterno";
-#else
-    const string BASE_PATH = @"C:\Users\maste\OneDrive\Documents";
-#endif
+    public static readonly string BASE_PATH;
 
-    public static readonly string CarnagesDir = Path.Combine(BASE_PATH, "Halo", "Carnages");
-    public static readonly string DatabaseDir = Path.Combine(BASE_PATH, "Halo", "DBStats DataBase");
+    static Paths()
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            BASE_PATH = "/media/pi/MiDiscoExterno";
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            BASE_PATH = @"C:\Users\maste\OneDrive\Documents";
+        }
+        else
+        {
+            throw new NotSupportedException("Operating system not supported.");
+        }
+    }
+
+    public static readonly string CarnagesDir = Path.Combine(BASE_PATH!, "Halo", "Carnages");
+    public static readonly string DatabaseDir = Path.Combine(BASE_PATH!, "Halo", "DBStats DataBase");
 
     public static readonly string SavedHashesPath = Path.Combine(
         AppContext.BaseDirectory, "Duplicates", "processed_hashes.json"
     );
 
     public static readonly string CustomizationPath = Path.Combine(
-        BASE_PATH, "Halo", "Discord Bot", "customization.xml"
+        BASE_PATH!, "Halo", "Discord Bot", "customization.xml"
     );
 }
